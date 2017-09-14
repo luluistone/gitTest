99c4ef4b96f3b79cf2b33e2687d7e2f42ed5b759
stone <stone@139.com>
2017-09-14 22:25:11 +0800

    commit twice

--- src/main/java/com/atguigu/git/test/Apple.java
+++ src/main/java/com/atguigu/git/test/Apple.java
@@ -4,7 +4,8 @@ public class Apple {
 	public static void main(String[] args) {
 		
 		System.out.println("hello apple");
-		System.out.println("stoneatguigu edit");
+		System.out.println("stoneatguigu edit twice");
+		
 	}
 
 }
